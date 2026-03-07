f5628cc271c43a7b515acb2aff4434da9ab32550
Prince04012004 <princekudiya2004@gmail.com>
2026-03-07 15:56:38 +0530

    Add new url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios  from "axios";
 
 const api=axios.create({
-    baseURL:"https://budget-box-backend-7zhw.onrender.com/api"
+    baseURL:"https://budget-box-backend-kmfx.onrender.com"
     // baseURL:"http://localhost:3000/api"
 })
 //Add token to header
